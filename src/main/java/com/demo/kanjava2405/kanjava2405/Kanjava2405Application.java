6ee8923cb846f9c625173b2e67216ea12cf949dd
horinoA <horinoakiko@gmail.com>
2024-05-18 08:00:26 +0900

    first commit やっていき(｡•̀ω-)☆

---
+++ src/main/java/com/demo/kanjava2405/kanjava2405/Kanjava2405Application.java
@@ -0,0 +1,13 @@
+package com.demo.kanjava2405.kanjava2405;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kanjava2405Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kanjava2405Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/kanjava2405/kanjava2405/Kanjava2405ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.kanjava2405.kanjava2405;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kanjava2405ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
