48ff16420663cb05ca86b0c813ddf6bb7cde15be
horinoA <horinoakiko@gmail.com>
2024-05-18 15:40:04 +0900

    reositoryクラス定義だけ

---
+++ src/main/java/com/demo/kanjava2405/kanjava2405/repository/personaldictInputRepository.java
@@ -0,0 +1,11 @@
+package com.demo.kanjava2405.kanjava2405.repository;
+
+import org.springframework.data.repository.Repository;
+
+import com.demo.kanjava2405.kanjava2405.model.personaldictInput;
+
+public interface personaldictInputRepository extends Repository<personaldictInput,Integer>{
+
+    
+    
+}
